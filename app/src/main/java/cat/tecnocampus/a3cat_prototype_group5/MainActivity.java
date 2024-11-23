86d3261fcc7154b85be7a52accc917bc06508819
DavidPujol <dpujol@edu.tecnocampus.cat>
2024-11-23 12:30:58 +0100

    Project create

---
+++ app/src/main/java/cat/tecnocampus/a3cat_prototype_group5/MainActivity.java
@@ -0,0 +1,12 @@
+package cat.tecnocampus.a3cat_prototype_group5;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu); // Ensure you have a corresponding XML layout file
+    }
+}
