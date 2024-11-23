5e680911470e4a9d252fa365a0b95279b5cc5d21
Maltas <107067687+Maaltas@users.noreply.github.com>
2024-11-23 15:05:58 +0100

    initialized some stuff

--- app/src/main/java/cat/tecnocampus/a3cat_prototype_group5/MainActivity.java
+++ app/src/main/java/cat/tecnocampus/a3cat_prototype_group5/MainActivity.java
@@ -2,11 +2,18 @@ package cat.tecnocampus.a3cat_prototype_group5;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_menu); // Ensure you have a corresponding XML layout file
+        setContentView(R.layout.main_menu);
+        loadFragment(new OptionFragment());
+    }
+    public void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
     }
 }
---
+++ app/src/main/java/cat/tecnocampus/a3cat_prototype_group5/OptionFragment.java
@@ -0,0 +1,7 @@
+package cat.tecnocampus.a3cat_prototype_group5;
+
+import androidx.fragment.app.Fragment;
+
+public class OptionFragment extends Fragment {
+    
+}
