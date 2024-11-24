4f15da9cbb62896e568b25be643303df9be03226
DavidPujol <dpujol@edu.tecnocampus.cat>
2024-11-24 13:15:54 +0100

    fragment creation

--- app/src/main/java/cat/tecnocampus/a3cat_prototype_group5/GameFragment.java
+++ app/src/main/java/cat/tecnocampus/a3cat_prototype_group5/GameFragment.java
@@ -1,4 +1,18 @@
 package cat.tecnocampus.a3cat_prototype_group5;
 
-public class GameFragment extends Fragment{
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class GameFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_game, container, false);
+    }
 }
