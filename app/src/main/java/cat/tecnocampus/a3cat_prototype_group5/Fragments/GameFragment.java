aebe199eede7d3096130ac1d5cce8a1766aa67e4
DavidPujol <dpujol@edu.tecnocampus.cat>
2024-11-28 20:02:54 +0100

    Game logic

--- app/src/main/java/cat/tecnocampus/a3cat_prototype_group5/Fragments/GameFragment.java
+++ app/src/main/java/cat/tecnocampus/a3cat_prototype_group5/Fragments/GameFragment.java
@@ -1,20 +1,91 @@
 package cat.tecnocampus.a3cat_prototype_group5.Fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
+import java.util.Random;
 import cat.tecnocampus.a3cat_prototype_group5.R;
 
 public class GameFragment extends Fragment {
 
+    private ImageButton btnBack, gameCanvas;
+    private TextView currentScore, currentRecord, endGameMessage, endGameScore;
+    private LinearLayout endGameBubble;
+    private Button btnPlayAgain, btnEnterRaffle;
+    private SharedPreferences sharedPreferences;
+    private int recordScore;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_game, container, false);
+        View view = inflater.inflate(R.layout.fragment_game, container, false);
+
+
+        btnBack = view.findViewById(R.id.btn_back);
+        gameCanvas = view.findViewById(R.id.game_canvas);
+        currentScore = view.findViewById(R.id.current_score_number);
+        currentRecord = view.findViewById(R.id.current_record_number);
+        endGameMessage = view.findViewById(R.id.end_game_message);
+        endGameScore = view.findViewById(R.id.end_game_score);
+        endGameBubble = view.findViewById(R.id.end_game_bubble);
+        btnPlayAgain = view.findViewById(R.id.btn_play_again);
+        btnEnterRaffle = view.findViewById(R.id.btn_enter_raffle);
+
+
+        sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+        recordScore = sharedPreferences.getInt("record_score", 0);
+        currentRecord.setText(String.valueOf(recordScore));
+
+
+        btnBack.setOnClickListener(v -> getActivity().onBackPressed());
+        gameCanvas.setOnClickListener(v -> handleGameCanvasClick());
+        btnPlayAgain.setOnClickListener(v -> resetGame());
+        btnEnterRaffle.setOnClickListener(v -> goToRaffleScreen());
+
+        return view;
+    }
+
+    private void handleGameCanvasClick() {
+        Random random = new Random();
+        int newScore = random.nextInt(46) + 10; // Generate random score between 10 and 55
+        currentScore.setText(String.valueOf(newScore));
+
+        if (newScore > recordScore) {
+            recordScore = newScore;
+            sharedPreferences.edit().putInt("record_score", recordScore).apply();
+            currentRecord.setText(String.valueOf(recordScore));
+            endGameMessage.setText(R.string.string_new_record_message);
+        } else {
+            endGameMessage.setText(R.string.string_game_over_message);
+        }
+
+        endGameScore.setText(R.string.score + newScore);
+        endGameBubble.setVisibility(View.VISIBLE);
+    }
+
+    private void resetGame() {
+        endGameBubble.setVisibility(View.GONE);
+        currentScore.setText("0");
+    }
+
+    private void goToRaffleScreen() {
+        RaffleInscriptionFragment raffleInscriptionFragment = new RaffleInscriptionFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt("record_score", recordScore);
+        raffleInscriptionFragment.setArguments(bundle);
+        getParentFragmentManager().beginTransaction()
+            .replace(R.id.fragment_container, raffleInscriptionFragment)
+            .addToBackStack(null)
+            .commit();
     }
 }
